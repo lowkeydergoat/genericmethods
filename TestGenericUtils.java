0b7215ad205d2dae83e004efd32f5430c82c87e2
stefan <skordov@student.tgm.ac.at>
2025-02-20 14:33:30 +0100

    test class

--- TestGenericUtils.java
+++ TestGenericUtils.java
@@ -3,5 +3,17 @@ import java.util.Arrays;
 public class TestGenericUtils {
 
     public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        GenericUtils.reverse(intArray);
+        System.out.println(Arrays.toString(intArray));
+
+        String[] strArray = {"yousef sheikho", "fabian heler", "lukas hecher", "maroi milkov"};
+        GenericUtils.reverse(strArray);
+        System.out.println(Arrays.toString(strArray));
+        Double[] doubleArray = {1.5, 2.5, 3.5};
+        System.out.println(GenericUtils.sum(doubleArray));
+
+        Integer[] integerArray = {10, 20, 30};
+        System.out.println(GenericUtils.sum(integerArray));
     }
 }
