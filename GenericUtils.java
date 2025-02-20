6aff3273be13d87401cd92406a223a1b441a2c6c
stefan <skordov@student.tgm.ac.at>
2025-02-20 14:33:40 +0100

    documentation

--- GenericUtils.java
+++ GenericUtils.java
@@ -1,4 +1,11 @@
 public class GenericUtils {
+
+    /**
+     * Reverses the elements of the array.
+     *
+     * @param <T>   generic elements in the array
+     * @param array the array to be reversed
+     */
     public static <T> void reverse(T[] array) {
         int left = 0;
         int right = array.length - 1;
@@ -11,6 +18,15 @@ public class GenericUtils {
             right--;
         }
     }
+
+    /**
+     * Calculates the sum of all elements in the array.
+     * The array must contain numbers.
+     *
+     * @param <T>   the type of elements in the array, must be a number
+     * @param array the array of numbers to sum of
+     * @return the sum of the elements as a double
+     */
     public static <T extends Number> double sum(T[] array) {
         double total = 0.0;
         for (int i = 0; i < array.length; i++) {
